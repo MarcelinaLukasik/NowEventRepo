0f7e384e8a4c0da28031767044bf778a93c50765
lukasikmarcelina91 <lukasikmarcelina91@gmail.com>
2023-01-14 14:56:21 +0100

    add more unit tests

--- NowEventGrande/NowEvent.Test/EventUnitTest.cs
+++ NowEventGrande/NowEvent.Test/EventUnitTest.cs
@@ -14,24 +14,25 @@ namespace NowEvent.Test
         private readonly IBudgetRepository _budgetRepository;
 
         [TestMethod]
-        public void Test_GetStatus()
+        public void TestGetStatus()
         {
-            var mockSet = new Mock<DbSet<Event>>();
-            var events = new List<Event>() {
-                new Event() {
-                    Id = 9999,
-                    Size = "Large",
-                    Type = "Festival",
-                    Name = "FestivalEvent",
-                    Status = "Incomplete"
+            // var mockSet = new Mock<DbSet<Event>>();
+            // var events = new List<Event>() {
+            //     new Event() {
+            //         Id = 9999,
+            //         Size = "Large",
+            //         Type = "Festival",
+            //         Name = "FestivalEvent",
+            //         Status = "Incomplete"
+            //
+            //     }
+            // };
 
-                }
-            };
+            // var queryable = events.AsQueryable();
+            // mockSet.As<IQueryable<Event>>().Setup(m => m.Expression).Returns(queryable.Expression);
             var options = new DbContextOptionsBuilder<AppDbContext>()
                 .UseInMemoryDatabase(databaseName: "NowEvent")
                 .Options;
-            var queryable = events.AsQueryable();
-            mockSet.As<IQueryable<Event>>().Setup(m => m.Expression).Returns(queryable.Expression);
             using (var context = new AppDbContext(options))
             {
                 context.Events.Add(new Event
@@ -55,9 +56,73 @@ namespace NowEvent.Test
         }
 
         [TestMethod]
-        public void Test_GetEventStartDate()
+        public void TestGetEventStartDate()
         {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "NowEvent")
+                .Options;
+            DateTime dateTocheck = new DateTime(2023, 12 , 23 ,06,00,00);
+            using (var context = new AppDbContext(options))
+            {
+                context.Events.Add(new Event
+                {
+                    Id = 99999,
+                    Size = "Large",
+                    Type = "Festival",
+                    Name = "FestivalEvent",
+                    Status = "Incomplete",
+                    EventStart = dateTocheck
 
+                });
+
+                context.SaveChanges();
+            }
+            using (var context = new AppDbContext(options))
+            {
+                EventRepository eventRepository = new EventRepository(context, _budgetRepository, _locationRepository);
+                var dateResult = eventRepository.GetEventStartDate(99999);
+                Assert.AreEqual(dateTocheck, dateResult);
+            }
+        }
+
+        [TestMethod]
+        public void TestSetEventDateAndTime()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "NowEvent")
+                .Options;
+            DateTime eventStart = new DateTime(2023, 12, 23, 06, 00, 00);
+            DateTime eventEnd = new DateTime(2023, 12, 23, 09, 00, 00);
+            DateTime eventDate = new DateTime(2023, 12, 23, 06, 00, 00);
+            Dictionary<string, string> formattedInfo = new Dictionary<string, string>()
+            {
+                ["Date"] = "2022 - 01 - 19T23:00:00.000Z",
+                ["StartTime"] = "04:00 AM",
+                ["EndTime"] = "08:00 AM"
+            };
+            using (var context = new AppDbContext(options))
+            {
+                context.Events.Add(new Event
+                {
+                    Id = 999999,
+                    Size = "Large",
+                    Type = "Festival",
+                    Name = "FestivalEvent",
+                    Status = "Incomplete",
+                    EventStart = eventStart,
+                    EventEnd = eventEnd,
+                    Date = eventDate
+
+                });
+
+                context.SaveChanges();
+            }
+            using (var context = new AppDbContext(options))
+            {
+                EventRepository eventRepository = new EventRepository(context, _budgetRepository, _locationRepository);
+                var date = eventRepository.SetEventDateAndTime(999999, formattedInfo);
+                Assert.IsFalse(date.Result);
+            }
         }
     }
 }
